bc1f2ddec39deec2f7c56d91eacb1578c2c6f9f8
Fatih Kurt <fatihkurt_1905@windowslive.com>
2021-04-17 13:56:05 +0300

    çalışmalar

---
+++ 03.ActionDelegeler/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace _03.ActionDelegeler
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Action action = new Action(SelamVer);
+
+
+            action.Invoke();
+
+
+            //Action<string> action2 = new Action<string>(SelamVer2);
+            //action2 += SelamVer3;
+            //action2.Invoke("Ahmet");
+
+            Action<string> action2 = new Action<string>(name =>
+            {
+
+                Console.WriteLine("selam"+ name);
+
+
+            });
+
+            action2.Invoke("Fatih");
+
+
+        
+        }
+
+
+        private static void SelamVer3(string name)
+
+        {
+            Console.WriteLine("Selam" + name+ "nasılsın");
+
+        }   
+        private static void SelamVer2(string name)
+
+            { Console.WriteLine("Selam"+ name);
+        }
+
+        private static void SelamVer()
+        {
+         Console.WriteLine("Selam Fatih");
+        }
+    }
+}
---
+++ AspProje1/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspProje1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspProje1.Controllers
+{
+    public class HomeController : Controller
+    {
+        //ysk.com.tr/Home/Index
+        public IActionResult Index()
+        {
+            //viewbag,viewdata,tempdata,model
+            ViewBag.Name = "Yavuz";
+            ViewData["Name"] = "Selim";
+            TempData["Name"] = "Kahraman";
+
+
+            Customer customer = new () { Age = 27, FirstName = "fatih kurt", LastName = "KURT" };
+            return View(customer);
+        }
+
+        public IActionResult Yavuz()
+        {
+
+            return View();
+        }
+
+    }
+}
